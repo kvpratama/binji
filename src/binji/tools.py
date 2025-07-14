9dc28883b94bdfa1d0e5ea6895d79121b0fd4959
kvpratama <kvpratama@gmail.com>
2025-07-14 11:17:40 +0900

    Update tools.py

--- src/binji/tools.py
+++ src/binji/tools.py
@@ -4,31 +4,32 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 @tool
 def search_tavily(query: str):
-    """ 
+    """
     Research tool to search the web for information and format the results.
-    
+
     Args:
         query (str): The search query.
-    
+
     Returns:
         str: The formatted search documents.
     """
     logger.info("Entered search_tavily function.")
     try:
         # time.sleep(15) # to prevent rate limit
-        
+
         # Search
         tavily_search = TavilySearch(max_results=3)
         search_docs = tavily_search.invoke(query)
         logger.info(f"Retrieved {len(search_docs)} documents from Tavily.")
-        
+
         # Format
         formatted_search_docs = "\n\n---\n\n".join(
             [
                 f'<Document href="{doc["url"]}"/>\n{doc["content"]}\n</Document>'
-                for doc in search_docs['results']
+                for doc in search_docs["results"]
             ]
         )
         logger.info("Formatted search documents.")
